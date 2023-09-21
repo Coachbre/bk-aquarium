1a1ab2e483601029c3e9bcac17b7b7e01456463d
Coachbre <coachbre.dev@gmail.com>
2023-09-21 12:16:15 -0400

    fish are being rendered to the dom

---
+++ scripts/Fish.js
@@ -0,0 +1,15 @@
+/**
+ *  Fish which renders individual fish objects as HTML
+ */
+export const Fish = (fishObj) => {
+  return `<article class="fish-card">
+				<div><img class="fish-image" src="images/${fishObj.image}" /></div>
+				<h3 class="fish-name">${fishObj.name}</h3>
+				<ul>
+					<li class="fish-details">${fishObj.species}</li>
+					<li class="fish-details">Length: ${fishObj.length}</li>
+					<li class="fish-details">Found: ${fishObj.found}</li>
+					<li class="fish-details">Diet: ${fishObj.diet}</li>
+				</ul>
+        	</article>`
+}
--- scripts/FishData.js
+++ scripts/FishData.js
@@ -9,83 +9,95 @@ const fishCollection = [
     length: "10 inches",
     found: "Pacific Ocean",
     diet: "algae",
+    image: "bluefish.jpg"
   },
   {
     name:"Bob",
     species:"Guppy",
     length:"3 inches",
     found: "Bali",
-    diet: "really small fish"
+    diet: "really small fish",
+    image: "pinkfish.jpg"
   },
   {
     name: "Jerry",
     species: "Swordfish",
     length:"80 inches",
     found:"Pacific Ocean",
-    diet: "shellfish"
+    diet: "shellfish",
+    image: "orangefish.jpg"
   },
   {
     name: "Predator",
     species: "Catfish",
     length: "20 inches",
     found: "Ocoee River",
-    diet: "tiny fish"
+    diet: "tiny fish",
+    image: "bluefish.jpg"
   },
   {
     name: "Sabre",
     species: "Angel Fish",
     length: "12 inches",
     found: "Amazon Basin",
-    diet: "brine shrimp"
+    diet: "brine shrimp",
+    image: "pinkfish.jpg"
   },
   {
     name: "Una",
     species: "Clownfish",
     length: "4 inches",
     found: "Atlantic Ocean",
-    diet: "plankton"
+    diet: "plankton",
+    image: "orangefish.jpg"
   },
   {
     name: "Vacuum",
     species: "Catfish",
     length: "24 inches",
     found: "Mississippi River",
-    diet: "everything"
+    diet: "everything",
+    image: "orangefish.jpg"
   },
   {
     name: "Ralph",
     species: "Smallmouth Bass",
     length:"14 inches",
     found: "Bobbies Fish",
-    diet: "minnows"
+    diet: "minnows",
+    image: "bluefish.jpg"
   },
   {
     name: "Whiskers",
     species: "Ictalurus furcatus",
     length: "39 inches",
     found: "Cumberland River",
-    diet: "anything"
+    diet: "anything",
+    image: "orangefish.jpg"
   },
   {
     name: "Bruno",
     species: "Tuna",
     length:"64 inches",
     found: "South Pacific",
-    diet: "shrimp"
+    diet: "shrimp",
+    image: "pinkfish.jpg"
   },
   {
     name: "Dorothy",
     species: "goldfish",
     length: "3 inches",
     found: "Petsmart",
-    diet: "algae"
+    diet: "algae",
+    image: "orangefish.jpg"
   },
   {
     name: "Trent",
     species: "Rainbow trout",
     length: "21 inches",
     found: "Lake Tahoe",
-    diet: "insects"
+    diet: "insects",
+    image: "pinkfish.jpg"
   }
 
 ]
